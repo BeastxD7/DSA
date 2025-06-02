b8b0da8de135238345f4c2ad84c5db243f3186c0
BeastxD7 <shashankdevadiga2003@gmail.com>
2025-06-02 18:24:25 +0530

    Sum of Odd Numbers | While Loop

---
+++ CPP/oddSum.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main () {
+
+    int number, start = 1, sum = 0; 
+
+    cout<<"Enter the Number: " <<endl;
+    cin>> number;
+
+    while (start <= number) {
+
+        if(start % 2 != 0) {
+            sum += start;
+        }
+
+        start++;
+
+    }
+
+    cout<<"sum = " <<sum<<endl;
+
+}
